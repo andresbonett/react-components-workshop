762973596a2b610aaed26e619dd55ccff0f0d8f5
Andres Bonett <andresbonettm@gmail.com>
2024-01-09 00:30:25 -0500

    Refactor styles and update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import './App.css'
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,17 +10,20 @@ function App() {
       <h1 className="ext-3xl font-bold underline">React Components Workshop</h1>
       <div className="card">
         <button
+          className="btn btn-primary"
           onClick={() => {
             setCount(count => count + 1)
           }}
         >
-          count is {count}
+          count is hello {count}
         </button>
         <p className="mt-8">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
