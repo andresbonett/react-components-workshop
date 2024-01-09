208fcc0ffee78a48332379757f1b5ce089b4977a
Andres Bonett <andresbonettm@gmail.com>
2024-01-08 23:00:57 -0500

    Add ESLint configuration and add linting script

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 
 import './App.css'
 
-function App() {
+function App () {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className='ext-3xl font-bold underline'>React Components Workshop</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => { setCount((count) => count + 1) }}>
           count is {count}
         </button>
         <p className='mt-8'>
