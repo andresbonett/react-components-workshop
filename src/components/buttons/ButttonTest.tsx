1065e81e20aabb056d8cf376377aec86bae3c2f6
Andres Bonett <andresbonettm@gmail.com>
2024-01-08 22:40:59 -0500

    Add ButtonTest component

---
+++ src/components/buttons/ButttonTest.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface ButtonTestProps {
+  onClick: () => void
+  size?: 'sm' | 'md' | 'lg'
+  children: React.ReactNode
+}
+
+export default function ButtonTest ({
+  onClick,
+  children,
+  size
+}: ButtonTestProps) {
+  return (
+    <button className={`btn btn-primary ${size && size}`} onClick={onClick}>
+      {children}
+    </button>
+  )
+}
